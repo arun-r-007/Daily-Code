c07ae4ff58ee7861fe9a013dbf8f6d0e19b751f3
arun-r-007 <arunakhil9715@gmail.com>
2026-05-17 21:12:36 +0530

    CodeChef

---
+++ Code Chef/Miscellaneous Linear Search.py
@@ -0,0 +1,23 @@
+# cook your dish here
+
+n = int(input())
+
+pairs = []
+
+for i in range(n):
+    a, b = map(int, input().split())
+    pairs.append([a, b])
+
+x, y = map(int, input().split())
+
+flag = "No"
+
+for a, b in pairs:
+    if (a == x and b == y) or (a == y and b == x):
+        flag = "Yes"
+        break
+
+print(flag)
+
+
+
