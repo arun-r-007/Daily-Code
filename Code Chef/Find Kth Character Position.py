78fdad97eca68538b37d064de9f3c0124d885ab5
arun-r-007 <arunakhil9715@gmail.com>
2026-05-18 21:21:22 +0530

    CodeChef

---
+++ Code Chef/Find Kth Character Position.py
@@ -0,0 +1,16 @@
+# cook your dish here
+s, c, k = input().split()
+
+k = int(k)
+
+count = 0
+
+for i in range(len(s)):
+    if s[i] == c:
+        count += 1
+
+        if count == k:
+            print(i)
+            break
+else:
+    print(-1)
